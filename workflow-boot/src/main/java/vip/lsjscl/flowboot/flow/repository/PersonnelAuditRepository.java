704c54a390f489f1dad9e54d1d0a3397d3388ac6
zhangxingju <1533195362@qq.com>
2025-02-13 19:05:38 +0800

    删除没用代码

--- workflow-boot/src/main/java/vip/lsjscl/flowboot/flow/repository/PersonnelAuditRepository.java
+++
@@ -1,7 +0,0 @@
-package vip.lsjscl.flowboot.flow.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import vip.lsjscl.flowboot.flow.entity.PersonnelAudit;
-
-public interface PersonnelAuditRepository extends JpaRepository<PersonnelAudit, Long> {
-} 
